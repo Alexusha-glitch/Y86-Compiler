15f040c669fbda7db6db64ae6d5eeda17cfc8e6e
Alexusha-glitch <syscon.alex@gmail.com>
2026-03-03 22:39:31 -0500

    Working on Swing UI

---
+++ Y86.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Y86 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Y86 Compiler");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setBackground(Color.darkGray);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        
+        JLabel title = new JLabel("Y86 Code");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(title);
+
+        JLabel Y86_Code = new JLabel("Y86 Code:");
+        panel.add(Y86_Code);
+
+        frame.setVisible(true);
+    }
+}
