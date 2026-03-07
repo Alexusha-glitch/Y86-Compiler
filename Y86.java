37477fc7407851a61ee3d3ced030020d73f46838
Alexusha-glitch <syscon.alex@gmail.com>
2026-03-06 23:35:25 -0500

    Updated translation

--- Y86.java
+++ Y86.java
@@ -48,18 +48,48 @@ public class Y86 {
         int current_pos = 0;
         String[] lines = code.split("\n");
         String out = "";
+        String temp = "";
+        String operator;
         for (String line : lines) {
-            String[] arr = line.split(" ");
-            out += "0x" + current_pos;
+            String[] arr = line.split(" |,");
+            out += "0x" + Integer.toHexString(current_pos) + ": ";
             if (dict.containsKey(arr[0])) { // If it is an operation
-
+                operator = arr[0];
+                out += dict.get(operator);
+                current_pos++;
+                for (int i = 1; i < arr.length; i++) {
+                    if (arr[i].charAt(0) == '%') { // If it is a register
+                        out += dict.get(arr[i]);
+                    } else if (operator.equals("mrmovl")) { // Odd case where registers are backwards
+                        temp = arr[i].split("\\(")[0];
+                        temp = Integer.toHexString(Integer.parseInt(temp));
+                        if (temp.length() == 1) {
+                            temp = "0" + temp;
+                        }
+                        temp = dict.get(arr[i].split("\\(")[1].substring(0, 4)) + temp;
+                        i = 2;
+                        temp = dict.get(arr[i]) + temp;
+                        out += temp; // Completes loop as it edits i value. Translates both
+                    } else if (Character.isDigit(arr[i].split("\\(")[0].charAt(0))) {
+                        temp = arr[i].split("\\(")[0];
+                        temp = Integer.toHexString(Integer.parseInt(temp));
+                        if (temp.length() == 1) {
+                            temp = "0" + temp;
+                        }
+                        temp = dict.get(arr[i].split("\\(")[1].substring(0, 4)) + temp;
+                        out += temp;
+                    }
+                }
             } else { // If it is an assembly directive
                 if (arr[0].charAt(0) == '.') {
+                    if (arr[0].contains(".pos")) {
 
+                    }
                 } else { // If it is a symbolic name for point of code ("main: ___")
 
                 }
             }
+            out += "\n";
         }
         return out;
     }
