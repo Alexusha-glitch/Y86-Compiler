3462219e542d4b2c2b2431b4f2596ad62f0818c5
Alexusha-glitch <syscon.alex@gmail.com>
2026-04-30 20:42:33 -0400

    Update PipelineFunctions.java

--- PipelineFunctions.java
+++ PipelineFunctions.java
@@ -1,4 +1,52 @@
 public class PipelineFunctions {
+    public static int[] fetch(String assembly) {
+        int[] ret = new int[6];
+
+
+
+        return ret;
+    }
+
+    public static int[] decode(int RrA, int RrB, int Resp) {
+        int[] ret = new int[3];
+
+        
+
+        return ret;
+    }
+
+    public static int[] execute(int ifun, int valA, int valB) {
+        int[] ret = new int[3];
+
+        
+
+        return ret;
+    }
+
+    public static int[] memory(int valA, int valE, int valP) {
+        int[] ret = new int[2];
+
+        
+
+        return ret;
+    }
+
+    public static int writeBack(int valE, int valM) {
+        int ret = 0;
+
+        
+
+        return ret;
+    }
+
+    public static int PC(int valP, int CC, int valC) {
+        int ret = 0;
+
+        
+
+        return ret;
+    }
+
     public static void main(String[] args) {
         
     }
